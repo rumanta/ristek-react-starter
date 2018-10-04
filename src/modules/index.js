f334d6a0d754aa4b4884b624bc278cdf0afae432
aselhid <asel.hidayat@gmail.com>
2018-10-04 20:06:30 +0700

    updated container generator

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable */
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import TestContainerReducer from 'containers/TestContainer';
+import TestPageReducer from 'containers/TestPage';
 
 export default combineReducers({
   'routing': routerReducer,
+  'testContainer': TestContainerReducer,
+  'testPage': TestPageReducer,
 });
